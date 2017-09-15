4db83f94c34165cd8d006f78708ba1ea14d292f8
Armando <salixonline@gmail.com>
2017-09-15 22:25:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ function divide(a, b) {
   return a / b;
 }
 
-function inc(a) {
-  return a += 1;
+function inc(n) {
+  return n += 1;
 }
 
-function dec(a) {
-  return a -= 1;
+function dec(n) {
+  return n -= 1;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
 }
